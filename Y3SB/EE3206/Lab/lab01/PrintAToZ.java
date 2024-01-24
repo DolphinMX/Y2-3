aa6feaf309029ba6c07973cfb582849ed1e18cd2
DolphinMX <91420117+DolphinMX@users.noreply.github.com>
2024-01-24 21:13:49 +0800

    2.0

---
+++ Y3SB/EE3206/Lab/lab01/PrintAToZ.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01;
+
+/**
+ *
+ */
+public class PrintAToZ {
+    public static void main(String[] args) {
+        for(int i=0; i<26; i++) {
+            System.out.print((char)('A' + i));
+        }
+    }
+}
